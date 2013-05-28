fc0990dd34241018f98a5e4a2c1e92753bcfa7ee
Andreas Pfeifer <andreas.pfeifer@gmx.at>
2013-05-28 16:54:42 +0200

    commit - changes

--- SecureDWApp/SecureDWExtension/src/dke/extension/logic/crypto/AESCryptEngineImpl.java
+++ SecureDWApp/SecureDWExtension/src/dke/extension/logic/crypto/AESCryptEngineImpl.java
@@ -22,11 +22,11 @@ import org.bouncycastle.crypto.paddings.PaddedBufferedBlockCipher;
 import org.bouncycastle.crypto.params.KeyParameter;
 import org.bouncycastle.crypto.params.ParametersWithIV;
 
-protected class AESCryptEngineImpl implements CryptEngine {
+public class AESCryptEngineImpl implements CryptEngine {
     private BlockCipher engine;
     private BlockCipherPadding padding;
     private BufferedBlockCipher cipher;
-    
+
     public AESCryptEngineImpl() {
         super();
         /*
@@ -43,27 +43,33 @@ protected class AESCryptEngineImpl implements CryptEngine {
            *   - ZeroBytePadding
            */
         padding = new PKCS7Padding();
-        cipher = new PaddedBufferedBlockCipher(new CBCBlockCipher(engine), padding);
+        cipher =
+                new PaddedBufferedBlockCipher(new CBCBlockCipher(engine), padding);
     }
 
     // Encrypts a string.
-    public byte[] encryptString(String data, byte[] iv) 
-        throws CryptoException, NoSuchAlgorithmException, InvalidKeySpecException {
-      if (data == null || data.length() == 0) {
-        return new byte[0];
-      }
 
-      return this.encrypt(data.getBytes(), iv);
+    public byte[] encryptString(String data, byte[] iv) throws CryptoException,
+                                                               NoSuchAlgorithmException,
+                                                               InvalidKeySpecException {
+        if (data == null || data.length() == 0) {
+            return new byte[0];
+        }
+
+        return this.encrypt(data.getBytes(), iv);
     }
-    
+
     // Encrypt arbitrary byte array with a given key
-    public synchronized byte[] encrypt(byte[] data, byte[] iv) 
-        throws CryptoException, NoSuchAlgorithmException, InvalidKeySpecException {
-      if (data == null || data.length == 0) {
-        return new byte[0];
-      }
-      
-      CipherParameters params = null;
+
+    public synchronized byte[] encrypt(byte[] data,
+                                       byte[] iv) throws CryptoException,
+                                                         NoSuchAlgorithmException,
+                                                         InvalidKeySpecException {
+        if (data == null || data.length == 0) {
+            return new byte[0];
+        }
+
+        CipherParameters params = null;
 
         try {
             params = this.getCipherParameters(AccessPreferences.getKey(), iv);
@@ -71,19 +77,22 @@ protected class AESCryptEngineImpl implements CryptEngine {
             //todo: log meldung für den anfang, später infomeldung für den user
         }
         cipher.reset();
-      cipher.init(true, params);
-      
-      return this.callCipher(data);
+        cipher.init(true, params);
+
+        return this.callCipher(data);
     }
-    
-  // Decrypts arbitrary data.
-    public synchronized byte[] decrypt(byte[] data, byte[] iv) 
-        throws CryptoException, NoSuchAlgorithmException, InvalidKeySpecException {
-      if (data == null || data.length == 0) {
-        return new byte[0];
-      }
-        
-      CipherParameters params = null;
+
+    // Decrypts arbitrary data.
+
+    public synchronized byte[] decrypt(byte[] data,
+                                       byte[] iv) throws CryptoException,
+                                                         NoSuchAlgorithmException,
+                                                         InvalidKeySpecException {
+        if (data == null || data.length == 0) {
+            return new byte[0];
+        }
+
+        CipherParameters params = null;
 
         try {
             params = this.getCipherParameters(AccessPreferences.getKey(), iv);
@@ -91,53 +100,56 @@ protected class AESCryptEngineImpl implements CryptEngine {
             //todo: log meldung für den anfang, später infomeldung für den user
         }
         cipher.reset();
-      cipher.init(true, params);
+        cipher.init(true, params);
 
-      return this.callCipher(data);
+        return this.callCipher(data);
     }
 
     // Decrypts a string that was previously encoded
     // using encryptString.
-    public String decryptString(byte[] data, byte[] iv) 
-        throws CryptoException, NoSuchAlgorithmException, InvalidKeySpecException {
-      if (data == null || data.length == 0) {
-        return "";
-      }
 
-      return new String(this.decrypt(data, iv));
+    public String decryptString(byte[] data, byte[] iv) throws CryptoException,
+                                                               NoSuchAlgorithmException,
+                                                               InvalidKeySpecException {
+        if (data == null || data.length == 0) {
+            return "";
+        }
+
+        return new String(this.decrypt(data, iv));
     }
-    
+
     // Private routine that does the gritty work.
+
     private byte[] callCipher(byte[] data) throws CryptoException {
-      int size = cipher.getOutputSize(data.length);
-      byte[] result = new byte[size];
-      
-      // de/encrypting
-      /*
+        int size = cipher.getOutputSize(data.length);
+        byte[] result = new byte[size];
+
+        // de/encrypting
+        /*
        * data = input
        * 0 = offset at which input data starts
        * data.length = number of bytes to be de/encrypted
        * result = resultarray
        * 0 = offset from which the output will be copied
-       * 
+       *
        * returns number of output bytes
        */
-      int olen = cipher.processBytes(data, 0, data.length, result, 0);
-      
-      // process last blocks in the buffer
-      olen += cipher.doFinal(result, olen);
-      
-      if (olen < size) {
-        byte[] tmp = new byte[olen];
-        System.arraycopy(result, 0, tmp, 0, olen);
-        result = tmp;
-      }
-      
-      return result;
+        int olen = cipher.processBytes(data, 0, data.length, result, 0);
+
+        // process last blocks in the buffer
+        olen += cipher.doFinal(result, olen);
+
+        if (olen < size) {
+            byte[] tmp = new byte[olen];
+            System.arraycopy(result, 0, tmp, 0, olen);
+            result = tmp;
+        }
+
+        return result;
     }
-    
-    private CipherParameters getCipherParameters(SecretKey key, byte[] iv) 
-        throws NoSuchAlgorithmException {
+
+    private CipherParameters getCipherParameters(SecretKey key,
+                                                 byte[] iv) throws NoSuchAlgorithmException {
         // setup cipher parameters with key and IV
         KeyParameter keyParam = new KeyParameter(key.getEncoded());
         return new ParametersWithIV(keyParam, iv);
